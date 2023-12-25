af4a5376ca4bac11f2d43017021cac5807699dc1
ibaydin <batuhanaydn1998@gmail.com>
2023-12-25 21:51:36 +0300

    test code

--- test.py
+++ test.py
@@ -67,27 +67,61 @@ class TestModel:
         # Predicted Joint Space
         with torch.no_grad():
             predicted_joint_space = self.ann_model(torch.tensor(test_data, dtype=torch.float32).to(self.args.device))
-        s2 = time.time()
+
         predicted_joint_space = predicted_joint_space.cpu().numpy()
+        s2 = time.time()
 
         # Calculate Target World Space from predicted joint space
         target_world_space = np.empty((self.args.test_num_samples, 3), dtype=np.float32)
+        test_data_for_fkine = []
 
         for idx, angles in enumerate(predicted_joint_space):
             pose = self.robot_model.fkine(angles)
             target_world_space[idx, :] = np.array([pose.t[0], pose.t[1], pose.t[2]], dtype=np.float32)
 
+        for idx, angles in enumerate(test_output):
+            pose = self.robot_model.fkine(angles)
+            test_data_for_fkine.append(pose)
+
+        # Inverse Kinematics solution
+        inverse_angles = np.empty((self.args.test_num_samples, 4), dtype=np.float32)
+        s3 = time.time()
+
+        for idx, target in enumerate(test_data_for_fkine):
+            # Set position
+            angles = self.robot_model.ikine_LM(target)
+            inverse_angles[idx, :] = angles.q
+        s4 = time.time()
+
+        target_world_space_inverse = np.empty((self.args.test_num_samples, 3), dtype=np.float32)
+
+        # Calculate Inverse Error
+        for idx, angles in enumerate(inverse_angles):
+            pose = self.robot_model.fkine(angles)
+            target_world_space_inverse[idx, :] = np.array([pose.t[0], pose.t[1], pose.t[2]], dtype=np.float32)
+
+        inverse_error = np.abs(target_world_space_inverse - target_world_space)
+
+
         # Calculate Error
         error = np.abs(target_world_space - test_data)
 
         # Print Results
-        print("Test Results")
+        print("Model Test Results")
         print("Run Time : {} ms".format((s2-s1)*1000))
         print("Mean Error: {}".format(np.mean(error) * 1000))
 
         print("X Error: {}".format(np.mean(error[:, 0]) * 1000))
         print("Y Error: {}".format(np.mean(error[:, 1]) * 1000))
         print("Z Error: {}".format(np.mean(error[:, 2]) * 1000))
+
+        print("Inverse Test Results")
+        print("Run Time : {} s".format((s4-s3)))
+        print("Mean Error: {}".format(np.mean(inverse_error) * 1000))
+
+        print("X Error: {}".format(np.mean(inverse_error[:, 0]) * 1000))
+        print("Y Error: {}".format(np.mean(inverse_error[:, 1]) * 1000))
+        print("Z Error: {}".format(np.mean(inverse_error[:, 2]) * 1000))
 
         # Save results
         results = dict()
@@ -100,9 +134,15 @@ class TestModel:
         results["x_error"] = np.mean(error[:, 0]) * 1000
         results["y_error"] = np.mean(error[:, 1]) * 1000
         results["z_error"] = np.mean(error[:, 2]) * 1000
+        results["ik_time"] = s4-s3
+        results["ik_error"] = inverse_error
+        results["ik_mean_error"] = np.mean(inverse_error) * 1000
+        results["ik_x_error"] = np.mean(inverse_error[:, 0]) * 1000
+        results["ik_y_error"] = np.mean(inverse_error[:, 1]) * 1000
+        results["ik_z_error"] = np.mean(inverse_error[:, 2]) * 1000
 
         model_name = self.args.checkpoint.split(".")[0]
-        np.save(f"results/test_results_{model_name}.npy", results)
+        # np.save(f"results/test_results_{model_name}.npy", results)
 
 
 
@@ -115,11 +155,11 @@ def main(args):
 
 if __name__ == "__main__":
     arguments = argparse.ArgumentParser()
-    arguments.add_argument("--dataset_path", type=str, default="dataset")
-    arguments.add_argument("--dataset_name", type=str, default="AL5D_50k")
+    arguments.add_argument( "--dataset_path", type=str, default="dataset")
+    arguments.add_argument("--dataset_name", type=str, default="AL5D_100k")
     arguments.add_argument("--seed", type=int, default=0)
-    arguments.add_argument("--test_num_samples", type=int, default=100)
-    arguments.add_argument("--checkpoint", type=str, default="model_sigmoid_mae_10k.pth")
+    arguments.add_argument("--test_num_samples", type=int, default=1000)
+    arguments.add_argument("--checkpoint", type=str, default="model_sigmoid.pth")
     arguments.add_argument("--device", type=str, default="cpu")
     arguments.add_argument("--activation", type=str, default="sigmoid")
 
